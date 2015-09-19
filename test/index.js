51ba47e7e175a472f9df36e18df2076f57452618
liyh <liyh14@ucweb.com>
2015-09-19 15:48:39 +0800

    功能完善
    [add] traverse 功能完善
    [add] 精灵图操作封装到函数
    [todo] 测试用例
    [todo] Base64 支持

--- test/index.js
+++ test/index.js
@@ -1,12 +1,11 @@
 'use strict';
-console.time('t')
+console.time('t');
 
 var spritesmith = require('spritesmith'),
 	fs = require('fs'),
 	path = require('path'),
 	cssparse = require('css'),
 	traverse = require('./traverse');
-
 
 var options = {
 	src: 'dest/css/origin.css',
@@ -15,105 +14,123 @@ var options = {
 	uriImg: '../images/',
 	prevTag: 'sprite-',
 	retina: true,
+	algorithm: 'binary-tree',
 	padding: 10
 };
 
+function build(options) {
+	fs.readFile(options.src, 'utf8', function(err, file) {
+		var ast = cssparse.parse(file);
 
-fs.readFile(options.src, 'utf8', function(err, file) {
-	var ast = cssparse.parse(file);
-
-	// 遍历节点
-	var cache = [];
-	traverse(ast, {
-		enter: function(node, parent) {
-			if (node.property && node.property.indexOf('background') > -1) {
-				cache.push({
-					node: node,
-					parent: parent
-				});
+		// 遍历节点
+		var cache = [];
+		traverse(ast, {
+			enter: function(node, parent) {
+				if (node.property && node.property.indexOf('background') > -1) {
+					cache.push({
+						node: node,
+						parent: parent
+					});
+				}
 			}
-		},
-		leave: function(node, parent) {}
-	});
+		});
 
-	// 提取 url，分组1为url，分组2为精灵图标记，分组三为精灵图名称
-	var urlReg = /(?:url\(['"]?([\w\W]+?)(?:\?(__)?([\w\W]+?))?['"]?\))/,
-		basePath = path.join(__dirname, path.dirname(options.src)),
-		sprites = {},
-		spritesUrl = {},
-		u, tag, i, l;
+		// 提取 url，分组1为url，分组2为精灵图标记，分组三为精灵图名称
+		var urlReg = /(?:url\(['"]?([\w\W]+?)(?:\?(__)?([\w\W]+?))?['"]?\))/,
+			basePath = path.join(__dirname, path.dirname(options.src)),
+			sprites = {},
+			spritesUrl = {},
+			u, tag, i;
 
-	// 过滤获取的节点
-	for (i = 0; i < cache.length; i++) {
-		u = urlReg.exec(cache[i].node.value);
-		tag = null;
+		// 过滤获取的节点
+		for (i = 0; i < cache.length; i++) {
+			u = urlReg.exec(cache[i].node.value);
+			tag = null;
 
-		if (u) {
-			if (u[2]) {
-				tag = u[3];
-				if (!sprites[tag]) {
-					sprites[tag] = [];
+			if (u) {
+				if (u[2]) {
+					tag = u[3];
+					if (!sprites[tag]) {
+						sprites[tag] = [];
+					}
+					if (!spritesUrl[tag]) {
+						spritesUrl[tag] = [];
+					}
+
+					sprites[tag].push(cache[i]);
+					spritesUrl[tag].push(path.join(basePath, u[1]));
+				} else {
+					cache.splice(i--, 1);
 				}
-				if (!spritesUrl[tag]) {
-					spritesUrl[tag] = [];
-				}
-
-				sprites[tag].push(cache[i]);
-				spritesUrl[tag].push(path.join(basePath, u[1]));
-			} else {
-				cache.splice(i--, 1);
 			}
 		}
-	}
 
-	function buildSprite(tag, src) {
-		spritesmith({
-			src: src,
-			padding: options.padding
-		}, function handleResult(err, result) {
-			var spriteSet = sprites[tag],
-				spriteUrl = spritesUrl[tag],
-				coordinates = result.coordinates,
-				properties = result.properties,
-				pow = options.retina ? 2 : 1,
-				colorReg = /#\w{3,6}|rgba?\(.+?\)/,
-				ceil = Math.ceil,
-				color, i, offsetX, offsetY;
+		var syncCount = 0;
+		for (i in spritesUrl) {
+			buildSprite(i, spritesUrl[i]);
+		}
 
-			// 修改 AST
-			for (i = 0; i < spriteSet.length; i++) {
-				color = colorReg.exec(spriteSet[i].node.value);
-				color = color ? color[0] + ' ' : '';
-				offsetX = -ceil(coordinates[spriteUrl[i]].x / pow) + 'px';
-				offsetY = -ceil(coordinates[spriteUrl[i]].y / pow) + 'px';
-				spriteSet[i].node.value = color + 'url(' + options.uriImg + options.prevTag + tag + '.png) ' + offsetX + ' ' + offsetY;
+		/**
+		 * 根据精灵图标志和图片地址数组，创建精灵图
+		 * @param tag {String} 精灵图标志，也是生成精灵图的文件名
+		 * @param src {Array} 图片地址数组
+		 */
+		function buildSprite(tag, src) {
+			spritesmith({
+				src: src,
+				algorithm: options.algorithm,
+				padding: options.padding
+			}, function handleResult(err, result) {
+				var spriteSet = sprites[tag],
+					spriteUrl = spritesUrl[tag],
+					coordinates = result.coordinates,
+					properties = result.properties,
+					pow = options.retina ? 2 : 1,
+					colorReg = /#\w{3,6}|rgba?\(.+?\)/,
+					resetBgReg = /background-[size|image|position]/,
+					ceil = Math.ceil,
+					color, i, offsetX, offsetY;
 
-				spriteSet[i].parent.push({
-					type: 'declaration',
-					property: 'background-size',
-					value: ceil(properties.width / pow) + 'px ' + ceil(properties.height / pow) + 'px'
-				});
-			}
+				// 修改 AST
+				for (i = 0; i < spriteSet.length; i++) {
+					color = colorReg.exec(spriteSet[i].node.value);
+					color = color ? color[0] + ' ' : '';
+					offsetX = -ceil(coordinates[spriteUrl[i]].x / pow) + 'px';
+					offsetY = -ceil(coordinates[spriteUrl[i]].y / pow) + 'px';
+					spriteSet[i].node.value = color + 'url(' + options.uriImg + options.prevTag + tag + '.png) ' + offsetX + ' ' + offsetY;
 
-			fs.writeFile(path.join(__dirname, options.destImg, options.prevTag + tag + '.png'), result.image, 'binary');
+					// 删除 background 有关样式
+					traverse(spriteSet[i].parent, {
+						enter: function(node) {
+							if (node.property && (resetBgReg.exec(node.property))) {
+								return this.sign.remove;
+							}
+						}
+					});
 
-			// @TODO 添加多任务管理，只创建一次 css
-			fs.writeFile(path.join(__dirname, options.dest), cssparse.stringify(ast), function(err) {
-				if (err) {
-					throw err;
+					// 插入 background-size
+					spriteSet[i].parent.push({
+						type: 'declaration',
+						property: 'background-size',
+						value: ceil(properties.width / pow) + 'px ' + ceil(properties.height / pow) + 'px'
+					});
 				}
+
+				fs.writeFile(path.join(__dirname, options.destImg, options.prevTag + tag + '.png'), result.image, 'binary');
+
+				if (++syncCount === spriteUrl.length) {
+					// 创建 css
+					fs.writeFile(path.join(__dirname, options.dest), cssparse.stringify(ast), function(err) {
+						if (err) {
+							throw err;
+						}
+					});
+					console.timeEnd('t');
+				}
+
 			});
+		}
+	});
+}
 
-			if (++TNT === spriteUrl.length) {
-				console.timeEnd('t');
-			}
-
-		});
-	}
-
-	var TNT = 0;
-	for (i in spritesUrl) {
-		buildSprite(i, spritesUrl[i]);
-	}
-
-});
+build(options);
--- test/traverse.js
+++ test/traverse.js
@@ -1,30 +1,42 @@
+'use strict';
 function Traverse(ast, options) {
 	this.enter = options.enter;
 	this.leave = options.leave;
+	this.isKill = false;
 	this.sign = {
 		next: 'NEXT',
 		stop: 'STOP',
 		skip: 'SKIP',
 		remove: 'REMOVE'
-	}
-	this.check(ast);
+	};
+	this.check(ast, null, null);
 }
 
 var fn = Traverse.prototype,
-	ignoreType = /comment|position/;
+	ignoreType = /position/;
 
 /**
- * 遍历对象
+ * 遍历对象或数组
  */
 fn.walker = function(node) {
 	var k = Object.keys(node),
 		len = k.length,
-		item, i;
+		item, ret, i;
+
+	if(this.isKill) {
+		return;
+	}
 
 	for (i = 0; i < len; i++) {
 		item = node[k[i]];
 		if (!ignoreType.test(k[i]) && item && typeof item === 'object') {
-			this.check(item, node, k[i]);
+			ret = this.check(item, node, k[i]);
+			if(ret === this.sign.stop) {
+				this.isKill = true;
+				break;
+			} else if(ret === this.sign.skip) {
+				break;
+			}
 		}
 	}
 };
@@ -33,37 +45,45 @@ fn.walker = function(node) {
  * 检查对象，并触发回调函数，同时提供 parent 和 node 参数
  */
 fn.check = function(node, parent, key) {
-	var ret;
-	parent = parent || null;
+	// 调用 enter 函数
+	var ret = this.enterFn(node, parent);
 
-	// 调用 enter
-	if (this.enterFn(node, parent)) {
+	if (ret === this.sign.skip) {
+		ret = this.leaveFn(node, parent);
+
+	} else if(ret === this.sign.stop) {
+		this.isKill = true;
+
+	} else if(ret === this.sign.remove) {
+		if(isArray(parent)) {
+			parent.splice(key, 1);
+		} else {
+			delete parent[key];
+		}
+
+	} else {
 		this.walker(node);
+		ret = this.leaveFn(node, parent);
 	}
-	this.leave && this.leave(node, parent);
+	return ret;
 };
 
 /**
  * 封装参数 enter
- * @TODO 添加分支重载，减少函数调用
  */
 fn.enterFn = function(node, parent) {
-	var ret = this.sign.next;
 	if (typeof this.enter === 'function') {
-		ret = this.enter.call(this, node, parent) || ret;
+		return this.enter.call(this, node, parent);
 	}
-	return ret;
 };
 
 /**
  * 封装参数 leave
  */
 fn.leaveFn = function(node, parent) {
-	var ret = null;
 	if (typeof this.leave === 'function') {
-		ret = this.leave(node, parent);
+		return this.leave.call(this, node, parent);
 	}
-	return ret;
 };
 
 function isArray(obj) {
