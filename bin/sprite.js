30294287bcf7692f21a4b00b1033d9dff45f0e28
liyh <liyh14@ucweb.com>
2015-09-23 00:29:23 +0800

    问题修复
    [add] css 数组支持
    [upd] 路径处理由主模块内转移到外部修正
    [upd] 整合 grunt 优化
    [todo] css 产出路径判断

--- bin/sprite.js
+++ bin/sprite.js
@@ -1,88 +1,101 @@
 // #!/usr/bin/env node
 
 var argv = require('yargs')
-  .option('s', {
-    alias : 'src',
-    demand: true,
-    describe: 'css file input path',
-    type: 'string'
-  })
-  .option('d', {
-    alias : 'dest',
-    demand: true,
-    describe: 'css file output path',
-    type: 'string'
-  })
-  .option('o', {
-  	alias: 'destImg',
-  	demand: false,
-  	describe: 'output image path',
-  	type: 'string'
-  })
-  .option('r', {
-  	alias: 'retina',
-    demand: false,
-    describe: 'retina or not, default to true',
-    type: 'boolean'
-  })
-  .option('padding', {
-    demand: false,
-    describe: 'padding between images, default to 10',
-    type: 'string'
-  })
-  .option('prevTag', {
-    demand: false,
-    describe: 'add before images\' filename, default to "sprite-"',
-    type: 'string'
-  })
-  .option('imgPath', {
-    demand: false,
-    describe: 'images path, default to "../images/"',
-    type: 'string'
-  })
-  .option('algorithm', {
-    demand: false,
-    describe: 'list algorithm, default to "binary-tree"',
-    type: 'string'
-  })
-  .argv;
-
+    .option('s', {
+        alias: 'src',
+        demand: true,
+        describe: 'css file input path',
+        type: 'string'
+    })
+    .option('d', {
+        alias: 'dest',
+        demand: true,
+        describe: 'css file output path',
+        type: 'string'
+    })
+    .option('o', {
+        alias: 'destImg',
+        demand: false,
+        describe: 'output image path',
+        type: 'string'
+    })
+    .option('r', {
+        alias: 'retina',
+        demand: false,
+        default: true,
+        describe: 'retina or not, default to true',
+        type: 'boolean'
+    })
+    .option('padding', {
+        demand: false,
+        describe: 'padding between images, default to 10',
+        type: 'string'
+    })
+    .option('prevTag', {
+        demand: false,
+        describe: 'add before images\' filename, default to "sprite-"',
+        type: 'string'
+    })
+    .option('imgPath', {
+        demand: false,
+        describe: 'images path, default to "../images/"',
+        type: 'string'
+    })
+    .option('algorithm', {
+        demand: false,
+        describe: 'list algorithm, default to "binary-tree"',
+        type: 'string'
+    })
+    .argv;
 
 var args = {
-	src: argv.s,
-	dest: argv.d,
-	destImg: argv.o,
-	imgPath: argv.imgPath,
-	prevTag: argv.prevTag,
-	retina: argv.retina,
-	algorithm: argv.algorithm,
-	padding: argv.padding
+    src: argv.s.split(','),
+    dest: argv.d,
+    destImg: argv.o,
+    imgPath: argv.imgPath,
+    prevTag: argv.prevTag,
+    retina: argv.retina,
+    algorithm: argv.algorithm,
+    padding: argv.padding
 }
 
-var sprite = require('../lib/index');
+var sprite = require('../lib/sprite'),
+    path = require('path');
+
 var options = {
-	src: 'test/dest/css/origin.css',
-	dest: 'test/dest/css/dest.css',
-	destImg: 'test/dest/images/',
-	imgPath: '../images/',
-	prevTag: 'sprite-',
-	retina: true,
-	algorithm: 'binary-tree',
-	padding: 10
+    src: ['test/dest/css/origin.css', 'test/dest/css/two.css'],
+    dest: 'test/dest/css/a',
+    destImg: 'test/dest/images/',
+    imgPath: '../images/',
+    prevTag: 'sprite-',
+    retina: true,
+    algorithm: 'binary-tree',
+    padding: 10
 };
 
-sprite.build(extend(options, args));
+extend(options, args);
+
+// 修正路径
+options.src = options.src.map(function(item) {
+    return path.join(process.cwd(), item);
+});
+
+options.dest = path.join(process.cwd(), options.dest);
+options.destImg = path.join(process.cwd(), options.destImg);
+
+
+sprite.build(options);
 
 function extend(origin) {
-	var args = Array.prototype.slice.call(arguments, 1),
-		obj, key, i;
-	for(i=0; i<args.length; i++) {
-		obj = args[i];
-		for(key in obj) {
-			if(obj[key] != null) {
-				origin[key] = obj[key];
-			}
-		}
-	}
-	return origin;
+    var args = Array.prototype.slice.call(arguments, 1),
+        obj, key, i;
+    for (i = 0; i < args.length; i++) {
+        obj = args[i];
+        for (key in obj) {
+            if (obj[key] != null) {
+                origin[key] = obj[key];
+            }
+        }
+    }
+    return origin;
 }
